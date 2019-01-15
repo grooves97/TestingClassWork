ce91058c14c54fa38a17ec03f618ff2f755a6404
Аслан Нурсеитов <grooves97@gmail.com>
2019-01-15 12:40:00 +0600

    FileChanged

--- Test1/Test1/Program.cs
+++ Test1/Test1/Program.cs
@@ -10,6 +10,27 @@ namespace Test1
     {
         static void Main(string[] args)
         {
+            var item = new ClassTest();
+            item.Name = "Хлеб"; // вызван set
+            string itemName = item.Name;// вызван get
+            Console.WriteLine(item.Name);//тоже get
+
+            var anotherItem = new ClassTest
+            {
+                Id = 1,
+                Name = "Носки",
+                Price = 100,
+                //  CreationDate = DateTime.Now тут это не нужно ведь она уже расписана в кунструкторе по умолчанию
+            };
+
+
+            //     anotherItem.ChangedDates = new DateTime[2];
+            anotherItem[0] = DateTime.Now;//индексатор позволяет обращаться к внутреннему объекту
+
+            int[] numbers = { 1, 2, 3, 4, 5 };//Array
+            numbers[0] = 12; //Array[] - ?
+
+            Array.Resize(ref numbers, 30);
         }
     }
 }
